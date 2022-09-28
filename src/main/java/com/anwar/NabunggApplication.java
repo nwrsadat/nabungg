f80b2c77ed67ef598aa10a8cb5bd747c90cffa23
Anwar Sadat <anwarsadat.juni@gmail.com>
2022-09-28 15:22:48 +0700

    First git init

---
+++ src/main/java/com/anwar/NabunggApplication.java
@@ -0,0 +1,13 @@
+package com.anwar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NabunggApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NabunggApplication.class, args);
+	}
+
+}
